d3299a07c5788eaba92f3833c4da83d13bb8cfb6
alecube <alejo.lopez.lecube@gmail.com>
2022-06-12 18:12:05 -0300

    Primer aplicacion de template blog

--- paginas/models.py
+++ paginas/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Paginas(models.Model):
     titulo = models.CharField(max_length=100)
     fecha = models.DateTimeField()
-    copete = models.CharField(max_length=255)
+    copete = models.CharField(max_length=500)
     cuerpo = models.CharField(max_length=2000)
     imagen = models.ImageField(upload_to='imagenes', blank=True)
     imagen_epigrafe = models.CharField(max_length=50, blank=True, null=True)    
