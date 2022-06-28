7a08e76de24fdc7e80accd803584db2c9db9877d
xxFALKORxx <arturoportillo@gmail.com>
2022-06-28 14:12:06 -0500

    ultimos cambios

--- perfiles/views.py
+++ perfiles/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from perfiles.models import Perfiles
 from perfiles.forms import Perfil_form
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.       
+# Create your views here. 
+@login_required
 def listar_perfiles(request):
     perfiles = Perfiles.objects.all()
     context = {'perfiles':perfiles}
     return render(request, 'perfiles.html', context=context)
 
+@login_required
 def crear_perfil(request):
     if request.method == 'GET':
         form = Perfil_form()
@@ -20,6 +23,7 @@ def crear_perfil(request):
         if form.is_valid():
             nuevo_perfil = Perfiles.objects.create(
                 nombre = form.cleaned_data['nombre'],
+                apellidos = form.cleaned_data['apellidos'],
                 habilitado = form.cleaned_data['habilitado'],
                 tipo = form.cleaned_data['tipo'],
                 imagen = form.cleaned_data['imagen'], 
