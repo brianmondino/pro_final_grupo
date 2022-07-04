229dda5efc59954faa42d9dc521972025535c599
brianmondino <brianmondino@gmail.com>
2022-07-04 19:51:09 -0300

    sin actualizar url

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from paginas.views import actualizar_pagina, listar_paginas, crear_pagina, buscar_pagina, detalle_pagina, borrar_pagina, valorar_pagina, listar_paginas2, actualiza_vista
+from paginas.views import listar_paginas, crear_pagina, buscar_pagina, detalle_pagina, borrar_pagina, valorar_pagina, listar_paginas2, actualiza_vista
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,7 +10,7 @@ urlpatterns =[
     path('buscar-pagina/', buscar_pagina, name = 'buscar_pagina'),
     path('detalle-pagina/<int:pk>/', detalle_pagina, name = 'detalle_pagina'),
     path('borrar-pagina/<int:pk>/', borrar_pagina, name = 'borrar_pagina'),
-    path('actualizar-pagina/<int:pk>/', actualizar_pagina, name = 'actualizar_pagina'),
+    #path('actualizar-pagina/<int:pk>/', actualizar_pagina, name = 'actualizar_pagina'),
     path('valorar-pagina/', valorar_pagina, name = 'valorar_pagina'),
     path('listar-paginas2/', listar_paginas2, name = 'listar_paginas2'),
     path('actualiza-vista/<int:pk>/', actualiza_vista, name='actualiza_vista'),
